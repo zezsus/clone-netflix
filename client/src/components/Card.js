82247b92944476461ebbb1167ab5c10f14614783
Sang <sang166464@huce.edu.vn>
2023-12-02 21:05:54 +0700

    update card slider

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import "../assets/styles/styles-components/Card.scss";
 import { useNavigate } from "react-router-dom";
-import video from "../assets/images/video.mp4";
+// import video from "../assets/images/video.mp4";
 import icons from "../utils/icons";
 
 const Card = ({ movieData, index, isLiked = false }) => {
@@ -14,75 +14,48 @@ const Card = ({ movieData, index, isLiked = false }) => {
     BiChevronDown,
   } = icons;
 
-  const [isHover, setIsHover] = useState(false);
   const navigate = useNavigate();
 
   return (
-    <div
-      className="card"
-      onMouseEnter={() => {
-        setIsHover(true);
-      }}
-      onMouseLeave={() => {
-        setIsHover(false);
-      }}>
-      <img
-        src={`https://image.tmdb.org/t/p/w500${movieData.image}`}
-        alt="movie"
-      />
-      {isHover && (
-        <div className="hover">
-          <div className="image-video-container">
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movieData.image}`}
-              alt="movie"
-              onClick={() => navigate("/player")}
-            />
-            
-            <video
-              src={video}
-              autoPlay
-              loop
-              muted
-              onClick={() => navigate("/player")}
-            />
+    <div className="card-container">
+      <div className="image-container">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movieData.image}`}
+          alt="movie"
+          onClick={() => navigate("/player")}
+        />
+      </div>
+
+      <div className="info-container d-flex flex-column">
+        <h5 className="name" onClick={() => navigate("/player")}>
+          {movieData.name}
+        </h5>
+
+        <div className="icons d-flex justify-content-between">
+          <div className="controls d-flex">
+            <FaPlayCircle title="play" onClick={() => navigate("/player")} />
+            <RiThumbUpFill title="Like" />
+            <RiThumbDownFill title="Dislike" />
+            {isLiked ? (
+              <BsCheck title="Remove From List" />
+            ) : (
+              <AiOutlinePlus title="Add to  my list" />
+            )}
           </div>
 
-          <div className="info-container d-flex flex-column">
-            <h3 className="name" onClick={() => navigate("/player")}>
-              {movieData.name}
-            </h3>
-
-            <div className="icons d-flex justify-content-between">
-              <div className="controls d-flex">
-                <FaPlayCircle
-                  title="play"
-                  onClick={() => navigate("/player")}
-                />
-                <RiThumbUpFill title="Like" />
-                <RiThumbDownFill title="Dislike" />
-                {isLiked ? (
-                  <BsCheck title="Remove From List" />
-                ) : (
-                  <AiOutlinePlus title="Add to  my list" />
-                )}
-              </div>
-
-              <div className="info">
-                <BiChevronDown title="More Info" />
-              </div>
-            </div>
-
-            <div className="genres d-flex">
-              <ul className="d-flex">
-                {movieData.genres.map((genre) => {
-                  return <li key={genre}>{genre}</li>;
-                })}
-              </ul>
-            </div>
+          <div className="info">
+            <BiChevronDown title="More Info" />
           </div>
         </div>
-      )}
+
+        {/* <div className="genres d-flex">
+          <ul className="d-flex">
+            {movieData.genres.map((genre) => {
+              return <li key={genre}>{genre}</li>;
+            })}
+          </ul>
+        </div> */}
+      </div>
     </div>
   );
 };
--- client/src/components/CardSlider.js
+++ client/src/components/CardSlider.js
@@ -1,61 +1,27 @@
 import React, { useRef, useState } from "react";
 import Card from "./Card";
 import "../assets/styles/styles-components/CardSlider.scss";
-import icons from "../utils/icons";
 
 const CardSlider = ({ data, title }) => {
-  const { AiOutlineLeft, AiOutlineRight } = icons;
-
-  const [showControls, setShowControls] = useState(false);
-  const [sliderPosition, setSliderPosition] = useState(0);
-  const listRef = useRef();
-
-  const handleDirecion = (direction) => {
-    console.log(direction);
-    let distance = listRef.current.getBoundingClientRect().x - 70;
-    if (direction === "left" && sliderPosition > 0) {
-      listRef.current.style.transform = `translateX(${230 + distance}px)`;
-      setSliderPosition(sliderPosition - 1);
-    }
-    if (direction === "right" && sliderPosition < 4) {
-      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
-      setSliderPosition(sliderPosition + 1);
-    }
-  };
+  // const [sliderPosition, setSliderPosition] = useState(0);
+  // const listRef = useRef();
 
   return (
-    <div
-      className="card-slider d-flex flex-column"
-      onMouseEnter={() => {
-        setShowControls(true);
-      }}
-      onMouseLeave={() => {
-        setShowControls(false);
-      }}>
-      <h1>{title}</h1>
+    <>
+      {data.length > 0 && (
+        <div className="card-slider d-flex flex-column">
+          <h2>{title}</h2>
 
-      <div className="wrapper">
-        <div
-          className={`slider-action left ${
-            !showControls ? "none" : ""
-          } d-flex justify-content-center align-items-center`}>
-          <AiOutlineLeft onClick={() => handleDirecion("left")} />
+          <div className="wrapper">
+            <div className="card-slider-container d-flex">
+              {data.map((movie, index) => {
+                return <Card movieData={movie} index={index} key={movie.id} />;
+              })}
+            </div>
+          </div>
         </div>
-
-        <div className="card-slider-container d-flex" ref={listRef}>
-          {data.map((movie, index) => {
-            return <Card movieData={movie} index={index} key={movie.id} />;
-          })}
-        </div>
-
-        <div
-          className={`slider-action right ${
-            !showControls ? "none" : ""
-          } d-flex justify-content-center align-items-center`}>
-          <AiOutlineRight onClick={() => handleDirecion("right")} />
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
